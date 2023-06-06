51a480ffe215cc2c48dc2a6af2bbcfd4bd34688a
ttriukhan <134487553+ttriukhan@users.noreply.github.com>
2023-06-06 13:46:28 +0300

    game & screen classes

--- core/src/com/marsik/MarsikGame.java
+++ core/src/com/marsik/MarsikGame.java
@@ -1,31 +1,25 @@
 package com.marsik;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.marsik.screens.PlayScreen;
 
-public class MarsikGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class MarsikGame extends Game {
+	public SpriteBatch batch;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(new PlayScreen(this));
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
