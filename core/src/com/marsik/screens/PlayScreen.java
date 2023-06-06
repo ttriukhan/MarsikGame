9ca9008f829713a12d2cf3b9ca9f209c962277c0
ttriukhan <134487553+ttriukhan@users.noreply.github.com>
2023-06-06 14:07:51 +0300

    map1

--- core/src/com/marsik/screens/PlayScreen.java
+++ core/src/com/marsik/screens/PlayScreen.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
@@ -20,11 +19,21 @@ public class PlayScreen implements Screen {
     private Viewport gamePort;
     private Hud hud;
 
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
     public PlayScreen(MarsikGame game) {
         this.game = game;
         gameCam = new OrthographicCamera();
         gamePort = new FitViewport(MarsikGame.V_WIDTH, MarsikGame.V_HEIGHT, gameCam);
         hud = new Hud(game.batch);
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("map1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gameCam.position.set(gamePort.getScreenWidth()/2, gamePort.getScreenHeight()/2, 0);
+
     }
 
     @Override
@@ -32,10 +41,24 @@ public class PlayScreen implements Screen {
 
     }
 
+    private void handleInput(float dt) {
+        if(Gdx.input.isTouched())
+            gameCam.position.x += 100 * dt;
+    }
+
+    public void update(float dt) {
+        handleInput(dt);
+        gameCam.update();
+        renderer.setView(gameCam);
+    }
+
     @Override
     public void render(float delta) {
+        update(delta);
         Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        renderer.render();
 
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
