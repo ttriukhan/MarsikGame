63c23253b95be6674cec473179e37cc6781f8094
ttriukhan <134487553+ttriukhan@users.noreply.github.com>
2023-06-06 21:57:41 +0300

    marsik's states

--- core/src/com/marsik/sprites/Marsik.java
+++ core/src/com/marsik/sprites/Marsik.java
@@ -1,28 +1,62 @@
 package com.marsik.sprites;
 
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.*;
 import com.marsik.MarsikGame;
 import com.marsik.screens.PlayScreen;
 
-import java.awt.*;
-
 public class Marsik extends Sprite {
+    public enum State {JUMPING, FALLING, STANDING, RUNNING};
+    public State currentState;
+    public State previousState;
 
     public World world;
     public Body b2body;
 
+    private float stateTimer;
+    private boolean runningRight;
+
     public Marsik(World world, PlayScreen screen) {
         super(new TextureRegion(screen.getTexture()));
         this.world = world;
+
+        currentState = State.STANDING;
+        previousState = State.STANDING;
+        stateTimer= 0;
+        runningRight = true;
+
         defineMarsik();
         setBounds(0, 0, 16 / MarsikGame.PPM, 32 / MarsikGame.PPM);
     }
 
     public void update(float dt) {
         setPosition(b2body.getPosition().x - getWidth()/2, b2body.getPosition().y - getHeight()/2);
+        previousState = currentState;
+        currentState = getState();
+        runningRight = isRunningRight();
+        checkStateTime(dt);
+    }
+
+    private void checkStateTime(float dt) {
+        if(currentState==previousState) stateTimer+=dt;
+        else stateTimer =0;
+    }
+
+    private boolean isRunningRight() {
+        if(b2body.getLinearVelocity().x ==0 && runningRight)
+            return true;
+        return b2body.getLinearVelocity().x > 0;
+    }
+
+    private State getState() {
+        if(b2body.getLinearVelocity().y >0 )
+            return State.JUMPING;
+        else if(b2body.getLinearVelocity().y <0)
+            return State.FALLING;
+        else if(b2body.getLinearVelocity().x!=0)
+            return State.RUNNING;
+        else return State.STANDING;
     }
 
     public void defineMarsik() {
